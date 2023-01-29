82e2fa20611820e613362d64d12a1f78ed22a7c7
kaguya3222 <maksimanisimov322@gmail.com>
2023-01-29 20:06:26 +0200

    feat: implement calculate function

--- src/calculate.test.ts
+++ src/calculate.test.ts
@@ -7,7 +7,20 @@ describe('calculate', () => {
     ['3 * 2', 6],
     ['4 / 2', 2],
     ['1 + 2 * 3', 7],
+    ['3 * 2 - 1 / 2', 5.5],
+    ['4+18/(9-3)', 7],
+    ['(5+3) * 6 - (2+3)', 43],
+    ['12 / 3 - (4 * 6) / 4 - 15', -17],
+    ['6/3+ 15  ', 17],
+    ['(13+13) * 8 - (3 + 6) / 15 + (4 * 6) / 4', 213.4],
   ])('should calculate %s', (expression, result) => {
     expect(calculate(expression)).toBe(result)
   })
+
+  test.each(['', '1^2', '11', '++', '()', '(+-)', '12'])(
+    'should throw an error if expression contains invalid characters: "%s"',
+    (expression) => {
+      expect(() => calculate(expression)).toThrowError()
+    }
+  )
 })
--- src/calculate.ts
+++ src/calculate.ts
@@ -1,33 +1,111 @@
-type Expression = string
+const operators = {
+  '+': {
+    precedence: 1,
+    operation: (operand1: number, operand2: number) => operand1 + operand2,
+  },
+  '-': {
+    precedence: 1,
+    operation: (operand1: number, operand2: number) => operand1 - operand2,
+  },
+  '*': {
+    precedence: 2,
+    operation: (operand1: number, operand2: number) => operand1 * operand2,
+  },
+  '/': {
+    precedence: 2,
+    operation: (operand1: number, operand2: number) => operand1 / operand2,
+  },
+} as const
 
-const parse = (expression: Expression) => {
-  const test = expression.replace(/\s/g, '').split('')
+type OperatorKey = keyof typeof operators
 
-  console.log(test)
-
-  const [first, operator, second] = test
-
-  return {
-    first,
-    operator,
-    second,
-  }
+const comparePrecedence = (operator1: OperatorKey, operator2: OperatorKey) => {
+  return operators[operator1].precedence >= operators[operator2].precedence
 }
 
-// TODO: implement shunting-yard algorithm
-
-export const calculate = (expression: Expression) => {
-  const { first, operator, second } = parse(expression)
-
-  if (operator === '+') {
-    return Number(first) + Number(second)
-  } else if (operator === '-') {
-    return Number(first) - Number(second)
-  } else if (operator === '*') {
-    return Number(first) * Number(second)
-  } else if (operator === '/') {
-    return Number(first) / Number(second)
-  }
+const isOperator = (token: string | number): token is OperatorKey => {
+  return token in operators
 }
 
-console.log(calculate('1 + 3 * 2 - 4 / 2'))
+const isOperand = (token: string | number): token is number => {
+  return !Number.isNaN(Number(token))
+}
+
+type Parenthesis = '(' | ')'
+
+const validateExpression = (expression: string) => {
+  const parsed = expression.replace(/\s/g, '').match(/(\d+|\+|-|\*|\/|\(|\))/g)
+
+  if (
+    !parsed ||
+    parsed.length === 0 ||
+    parsed.every((token) => !isOperand(token)) ||
+    parsed.every((token) => !isOperator(token))
+  ) {
+    throw new Error('Invalid expression')
+  }
+
+  return parsed
+}
+
+const parse = (expression: string) => {
+  const tokens = validateExpression(expression)
+
+  const stack: Array<OperatorKey | Parenthesis> = []
+  const output: Array<number | OperatorKey> = []
+
+  tokens.forEach((token) => {
+    const isOperand = !Number.isNaN(Number(token))
+
+    if (isOperand) {
+      output.push(Number(token))
+    }
+
+    if (isOperator(token)) {
+      while (
+        stack.length &&
+        isOperator(stack[stack.length - 1]) &&
+        comparePrecedence(stack[stack.length - 1] as OperatorKey, token)
+      ) {
+        output.push(stack.pop() as OperatorKey)
+      }
+      stack.push(token)
+    }
+
+    if (token === '(') {
+      stack.push(token)
+    }
+
+    if (token === ')') {
+      while (stack[stack.length - 1] !== '(') {
+        output.push(stack.pop() as OperatorKey)
+      }
+
+      stack.pop()
+    }
+  })
+
+  while (stack.length) output.push(stack.pop() as OperatorKey)
+
+  return output
+}
+
+export const calculate = (expression: string) => {
+  const rpn = parse(expression)
+  const stack: Array<number> = []
+
+  rpn.forEach((token) => {
+    const isOperand = typeof token === 'number'
+
+    if (isOperand) {
+      stack.push(token)
+    } else if (isOperator(token)) {
+      const operand2 = stack.pop() as number
+      const operand1 = stack.pop() as number
+      const result = operators[token].operation(operand1, operand2)
+      stack.push(result)
+    }
+  })
+
+  return stack.pop() as number
+}
